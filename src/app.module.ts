eac289d6a02af0caa9876339af21228cdc2bf360
Yodahe Teshome <jodahe2001@gmail.com>
2025-06-25 20:23:56 +0300

    modules added gi=ot issue on hotel module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,20 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { HotelModule } from './hotel/hotel.module';
 import config from '../mikro-orm.config';
+import { HotelPersistenceEntity } from './hotel/infrastructure/adapters/orm';
+import { UserModule } from './user/user.module';
+import { RoomModule } from './room/room.module';
+import { RoomTypeModule } from './roomType/roomType.module';
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     MikroOrmModule.forRoot(config),
+    HotelModule,
+    UserModule,
+    RoomModule,
+    RoomTypeModule,
   ],
   controllers: [AppController],
   providers: [AppService],
